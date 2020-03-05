d80a1aacfb727508e7617bfb7cde32e4f371e695
ArturZybin <artur.zybin.work@gmail.com>
2020-03-05 16:45:59 +0700

    switch SearchBar from function to class to add searching by enter

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,74 +1,78 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
 import Filter from './Filter';
 
 
-function SearchBar(props) {
-   const [isFilterClosed, setFilterClosed] = useState(true);
-   const [isCentered, setIsCentered] = useState(true);
-
-   const [text, setText] = useState();
-   const [type, setType] = useState();
-   const [genre, setGenre] = useState();
-   const [year, setYear] = useState();
-
-
-   const textInput = useRef();
-   const searchButton = useRef();
-   useEffect(() => {
-      const triggerSearch = (e) => {
-         if (e.keyCode !== 13) return;
-         if (document.activeElement !== textInput.current
-            && document.activeElement !== searchButton.current) {
-            return
-         }
-
-         handleSearch();
-      }
-
-      document.addEventListener('keypress', triggerSearch);
-
-      return (() => {
-         document.removeEventListener('keypress', triggerSearch);
-      })
-   }, [])
-
-   
-   const handleSearch = () => {
-      if (isCentered && textInput.current.value) {
-         setIsCentered(false);
-      }
-      props.handleSearch({
-         text, type, genre, year
-      })
+class SearchBar extends React.Component {
+   state= {
+      isFilterClosed: true,
+      isCentered: true,
+      text: null,
+      type: null,
+      genre: null,
+      year: null
    }
 
-   return (
-      <div className={isCentered ? "searchbar searchbar_centered" : "searchbar"}>
-         <input
-            className="searchbar__input"
-            type="text"
-            placeholder="Search movies"
-            autoFocus={true}
-            onChange={(e) => setText(e.target.value)}
-            ref={textInput}
-         />
-         <button
-            className="searchbar__filter-button searchbar__button"
-            onClick={() => setFilterClosed(!isFilterClosed)}
-         />
-         <button
-            className="searchbar__search-button searchbar__button"
-            onClick={handleSearch}
-            ref={searchButton}
-         />
-         <Filter
-            isClosed={isFilterClosed}
-            handleTypeChange={setType}
-            handleGenreChange={setGenre}
-            handleYearChange={setYear}
-         />
-      </div>
-   )
+   triggerEnterKeySearch = (e) => {
+      if (e.keyCode !== 13) return;
+      document.querySelector('.searchbar__search-button').click();
+   }
+
+   setupEnterSearch = (e) => {
+      document.addEventListener('keypress', this.triggerEnterKeySearch);
+   }
+
+   cleanUpEnterSearch = (target) => {
+      document.removeEventListener('keypress', this.triggerEnterKeySearch);
+      target.onBlur = null;
+   }
+
+   
+   handleSearch = () => {
+      const {isCentered, text, type, genre, year} = this.state;
+
+      if (isCentered && text) {
+         this.setState({ isCentered: false });
+      }
+
+      this.props.handleSearch({
+         text, type, genre, year
+      })
+      console.log({
+         text, type, genre, year
+      }, this.state.isFilterClosed)
+   }
+
+   render() {
+      const {isCentered, isFilterClosed} = this.state;
+
+      return (
+         <div className={isCentered ? "searchbar searchbar_centered" : "searchbar"}>
+            <input
+               className="searchbar__input"
+               type="text"
+               placeholder="Search movies"
+               autoFocus={true}
+               onChange={(e) => this.setState({ text: e.target.value })}
+               onFocus={this.setupEnterSearch}
+               onBlur={this.cleanUpEnterSearch}
+            />
+            <button
+               className="searchbar__filter-button searchbar__button"
+               onClick={() => this.setState({ isFilterClosed: !isFilterClosed })}
+            />
+            <button
+               className="searchbar__search-button searchbar__button"
+               onClick={this.handleSearch}
+            />
+            <Filter
+               isClosed={isFilterClosed}
+               handleTypeChange={(data) => this.setState({ type: data })}
+               handleGenreChange={(data) => this.setState({ genre: data })}
+               handleYearChange={(data) => this.setState({ year: data })}
+            />
+         </div>
+      )
+   }
 }
 
 export default SearchBar;
