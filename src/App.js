12e44a55073d96b2151b3ca3d571ef297ff40e6b
Artur Zybin <artur.zybin.work@gmail.com>
2020-03-06 13:00:02 +0700

    add fake json response gor testing while creating movies list

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import SearchBar from './Components/SearchBar';
 import ThemeToggler from './Components/ThemeToggler';
+import MoviesList from './Components/MoviesList';
 
 import 'normalize.css';
 import './styles/style.scss';
@@ -10,6 +11,9 @@ import './styles/style.scss';
 class App extends React.Component{
   state = {
     theme: 'light',
+    startNewSearch: false,
+    searchInfo: {},
+    showMoviesList: false,
   }
 
 
@@ -19,16 +23,32 @@ class App extends React.Component{
   }
 
 
-  search = (params) => {
-
+  startSearching = async (data) => {
+    this.setState({
+      startNewSearch: true,
+      searchData: data
+    });
   }
 
 
   render() {
+    const { showMoviesList, searchData, startNewSearch } = this.state;
+
     return (
       <main id="app-root" className={`theme-${this.state.theme}`} >
         <ThemeToggler handleToggle={this.toggleTheme} />
-        <SearchBar handleSearch={this.search} />
+        <SearchBar handleSearch={() => {
+          this.startSearching();
+          this.setState({ showMoviesList: true })
+        }}/>
+
+        {showMoviesList &&
+        <MoviesList
+          data={searchData}
+          startNewSearch={startNewSearch}
+          handleSeachStart={ () => this.setState({ startNewSearch: false }) }
+        />
+        }
       </main>
     )
   }
