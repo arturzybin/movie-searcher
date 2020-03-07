d672a778b6f04b7d146a197914dedffb0129274e
ArturZybin <artur.zybin.work@gmail.com>
2020-03-07 22:01:08 +0700

    Updates

---
+++ precache-manifest.7743bdcb662fe4aa8659282f1f26e931.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e1d228671c1b2ac45f32e39d044786e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81826c0d61fe2231447c",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "40982659eed3a9fb551d",
+    "url": "/static/css/main.76ffdb82.chunk.css"
+  },
+  {
+    "revision": "81826c0d61fe2231447c",
+    "url": "/static/js/2.8a85c79d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.8a85c79d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40982659eed3a9fb551d",
+    "url": "/static/js/main.c50cd2a0.chunk.js"
+  },
+  {
+    "revision": "d4058f64ce6bd4f6d92c",
+    "url": "/static/js/runtime-main.3baac50b.js"
+  },
+  {
+    "revision": "1b75c61a6bda00b833e87d4311737402",
+    "url": "/static/media/searcher.1b75c61a.svg"
+  },
+  {
+    "revision": "d477413f5e6dd5285727c70c757697d3",
+    "url": "/static/media/settings.d477413f.svg"
+  }
+]);
